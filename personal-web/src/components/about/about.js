6df218596fd3cd423fbbb9aa045220516c210adc
jess4lynwang@gmail.com <jess4lynwang@gmail.com>
2020-11-26 23:52:49 -0800

    progress on about page

--- personal-web/src/components/about/about.js
+++ personal-web/src/components/about/about.js
@@ -10,6 +10,17 @@ function About() {
             Economics at the University of California, Santa Cruz. My interests lie in 
             full-stack web development, machine learning, and data science. In my spare time
             I love to draw, read, play volleyball and water polo, and watch YouTube.</div>
+            <div className="about-header"><div>skills</div></div>
+            <div className="skills">
+              <div>Python: 5/5</div>
+              <div>C++: 3/5</div>
+              <div>HTML/CSS: 5/5</div>
+              <div>JavaScript: 4/5</div>
+            </div>
+            <div className="about-header"><div>current involvements</div></div>
+            <div className="about-link"><div>Tech4Good</div></div>
+            <div className="about-link"><div>NeuroTechSC</div></div>
+            <div className="about-link"><div>Wave</div></div>
         </div>
       </div>
     );
