db7b77ebced491cf38b4f47723f7dff25cd14d87
jess4lynwang@gmail.com <jess4lynwang@gmail.com>
2020-12-12 01:17:27 -0800

    pushed updates

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Home from './components/home/home.js';
 import About from './components/about/about.js';
+import Projects from './components/projects/projects.js';
+import Nav from './components/nav/nav.js';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
--- personal-web/src/components/about/about.js
+++ personal-web/src/components/about/about.js
@@ -2,7 +2,7 @@ import './about.css';
 
 function About() {
     return (
-      <div className="about">
+      <div className="about" id="about">
         <div className="cluster">
             <div className="about-title">Hi!</div>
             <div className="about-desc">My name is Jessalyn Wang, and I am a second year
@@ -12,15 +12,16 @@ function About() {
             I love to draw, read, play volleyball and water polo, and watch YouTube.</div>
             <div className="about-header"><div>skills</div></div>
             <div className="skills">
-              <div>Python: 5/5</div>
-              <div>C++: 3/5</div>
-              <div>HTML/CSS: 5/5</div>
-              <div>JavaScript: 4/5</div>
+              <div><i className="fab fa-python"></i></div>
+              <div><i className="fab fa-css3-alt"></i></div>
+              <div><i className="fab fa-html5"></i></div>
+              <div><img src="https://img.icons8.com/ios-filled/50/000000/c-plus-plus-logo.png" alt="c++ icon"/></div>
+              <div><i className="fab fa-js-square"></i></div>
             </div>
             <div className="about-header"><div>current involvements</div></div>
-            <div className="about-link"><div>Tech4Good</div></div>
-            <div className="about-link"><div>NeuroTechSC</div></div>
-            <div className="about-link"><div>Wave</div></div>
+            <a href="https://tech4good.soe.ucsc.edu/#/" rel="noreferrer" target="_blank" className="about-link tech4good"><div>Tech4Good</div></a>
+            <a href="https://neurotech.ucsc.edu/" rel="noreferrer" target="_blank" className="about-link ntxsc"><div>NeuroTechSC</div></a>
+            <a href="https://www.wavelf.org/" rel="noreferrer" target="_blank" className="about-link wave"><div>Wave</div></a>
         </div>
       </div>
     );
